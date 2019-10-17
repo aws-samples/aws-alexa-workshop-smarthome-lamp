699298fe15971987d274a946d6c1f2c3f3240758
Massimiliano Angelino <massi.ang@gmail.com>
2019-10-17 12:08:31 +0200

    fixed semicolon

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 module.exports = {
     thingName: '', // From the IoT Console https://console.aws.amazon.com/iot/home?region=us-east-1#/thinghub
     iotEndpoint: '', // From the IoT Console https://console.aws.amazon.com/iot/home?region=us-east-1#/settings
-    deviceBindingUrl = '' // From Amplify console https://console.aws.amazon.com/amplify/home?region=us-east-1#/
+    deviceBindingUrl: '' // From Amplify console https://console.aws.amazon.com/amplify/home?region=us-east-1#/
 }
